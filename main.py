4b90a3e113454a538cb4bd2732ee6ed052e2af43
Finn <fjlvanmontfort@gmail.com>
2022-11-07 17:48:17 +0000

    new

---
+++ main.py
@@ -0,0 +1,111 @@
+import sys
+import reporting
+import csv
+import os
+# This is a template. 
+# You should modify the functions below to match
+# the signatures determined by the project specification
+
+
+def main_menu():
+    """
+    1) User input assigned to the choice variable
+    2) Match statement checks each case and executes accordingly and if the input doesnt match it outputs an error message and returns to main menu
+    """
+    csv_to_array("Pollution-London Harlington.csv")
+    choice = input("Main Menu:"
+                +"\n\tR - Access to PR module"
+                +"\n\tI - Access to MI module"
+                +"\n\tM - Access to RM module"
+                +"\n\tA - Print the about text"
+                +"\n\tQ - Quit"
+                +"\nChoice:").upper()
+    match(choice):
+        case "R":
+            reporting_menu()
+        case "I":
+            intelligence_menu()
+        case "M":
+            monitoring_menu()
+        case "A":
+            about()
+        case "Q":
+            quit()
+        case _ :
+            print("Error please try again\nRemember to us uppercase letters")
+            main_menu()
+
+
+def monitoring_menu():
+    """Your documentation goes here"""
+    choice = input("Monitoring Menu:"
+                +"\nChoice:")
+
+def reporting_menu():
+    """Your documentation goes here"""
+    choice = input("Reporting Menu:"
+        +"\n\tDA - Access to daily average"
+        +"\n\tDM - Access to daily median"
+        +"\n\tHA - Access to hourly average"
+        +"\n\tMA - Access to monthly average"
+        +"\n\tPH - Access to peak hour of polution for a given date"
+        +"\n\tDA - Access to daily average"
+        +"\n\tM - Main Menu"
+        +"\nChoice:")
+    match(choice):
+        case "DA":
+            monitoring_station = input("Input monitoring station: ")
+            pollutant = input("Input polluntant: ")
+            data = csv_to_array("Pollution-"+monitoring_station+".csv")
+
+            reporting.daily_average(data,monitoring_station,pollutant)
+        case "DM":
+            reporting.daily_median()
+        case "HA":
+            reporting.hourly_average()
+        case "MA":
+            reporting.monthly_average()
+        case "PH":
+            reporting.peak_hour_date()
+        case "M":
+            main_menu()
+        case _ :
+            print("Error please try again\nRemember to us uppercase letters")
+            reporting_menu()
+
+def intelligence_menu():
+    """Your documentation goes here"""
+    choice = input("Intelligence Menu:"
+                +"\nChoice:")
+
+def about():
+    """Your documentation goes here"""
+    print("Module: ECM1400 \nCandidate Number: ???")
+    return main_menu()
+
+def quit():
+    """Your documentation goes here"""
+    sys.exit()
+
+def csv_to_array(filename):
+    """
+    Turns csv file into array
+    """
+    cwd = os.getcwd()
+    rows = []
+    with open(cwd+'\\data\\'+filename, 'r') as csvfile:
+        csvreader = csv.reader(csvfile)
+        for row in csvreader:
+            rows.append(row)
+    print("Total no. of rows: %d"%(csvreader.line_num))  
+    print(len(rows))
+    print('\nRows are:\n')
+    for row in rows[:5]:
+        for col in row:
+            print("%100s"%col,end=" "),
+        print('\n')
+
+
+
+if __name__ == '__main__':
+    main_menu()
---
+++ reporting.py
@@ -0,0 +1,48 @@
+import numpy
+# This is a template. 
+# You should modify the functions below to match
+# the signatures determined by the project specification
+
+
+def daily_average(data, monitoring_station:str, pollutant:str):
+    """Parameters:                                                
+        data = array 2D?
+        monitoring_station = String of the name of the monitoring station
+        polluntant = "no" = Nitric Oxide "PM10" = Inhalable partical matter <= 10µm  "PM25" = Inhalable partical matter <= 2.5µm        
+    """
+
+
+    daily_avg = []
+
+    return daily_avg
+    ## Your code goes here
+
+def daily_median(data, monitoring_station, pollutant):
+    """Your documentation goes here"""
+    
+    ## Your code goes here
+def hourly_average(data, monitoring_station, pollutant):
+    """Your documentation goes here"""
+    
+    ## Your code goes here
+def monthly_average(data, monitoring_station, pollutant):
+    """Your documentation goes here"""
+    
+    ## Your code goes here
+def peak_hour_date(data, date, monitoring_station,pollutant):
+    """Your documentation goes here"""
+    
+    ## Your code goes here
+
+def count_missing_data(data,  monitoring_station,pollutant):
+    """Your documentation goes here"""
+    
+    ## Your code goes here
+
+def fill_missing_data(data, new_value,  monitoring_station,pollutant):
+    """Your documentation goes here"""
+    
+    ## Your code goes here
+
+
+
