c68c1ad94fe5c1b2265d5e31bf31337e4cb42db6
TheCheesyWiggle <fjlvanmontfort@gmail.com>
2022-11-22 13:13:25 +0000

    started connected components

--- intelligence.py
+++ intelligence.py
@@ -19,6 +19,7 @@ def find_red_pixels(*args,**kwargs):
             if y[0]> upper_threshold and y[1]<lower_threshold and y[2] < lower_threshold:
                 array[i][j] = 1
     io.imsave("data//map-red-pixels.jpg",array)
+    return array
     
 
 def find_cyan_pixels(*args,**kwargs):
@@ -35,18 +36,33 @@ def find_cyan_pixels(*args,**kwargs):
         for j, y in enumerate(x):
             if y[0]< lower_threshold and y[1]>upper_threshold and y[2] > upper_threshold:
                 array[i][j] = 1
-    print(array)
     io.imsave("data//map-cyan-pixels.jpg",array)
+    return array
 
 
 def detect_connected_components(*args,**kwargs):
     """Your documentation goes here"""
-    # Your code goes here
+    IMG = args[0]
+    MARK = np.zeros(IMG.shape())
+    Q = np.empty()
+    for x, i in enumerate(IMG):
+        for y, j in enumerate(i):
+            if j == 1 and MARK[x][y]==0:
+                MARK[x][y] = 1
+                Q = np.append(Q,j)
+                while len(Q) == 0:
+                    q = Q[0]
+                    Q = np.delete(Q,0)
+                    #for loop 
 
 def detect_connected_components_sorted(*args,**kwargs):
-    """Your documentation goes here"""
+    """Your documentation goesnp.delete)
+    for each here"""
     # Your code goes here
 
+"""
 map_filename = "data\\map.png"
 find_red_pixels(map_filename, upper_threshold=100, lower_threshold=50)
 find_cyan_pixels(map_filename, upper_threshold=100, lower_threshold=50)
+"""
+
