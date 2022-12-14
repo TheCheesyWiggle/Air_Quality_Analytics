252741987c1501afe5625a7c596a89fc37c78d0b
TheCheesyWiggle <fjlvanmontfort@gmail.com>
2022-12-14 16:41:16 +0000

    vibes

--- test_intelligence.py
+++ test_intelligence.py
@@ -5,4 +5,12 @@ def test_findredpixels_1():
     assert type(find_red_pixels("data//map.png",upper_threshold=100,lower_threshold=50)) == np.ndarray
 
 def test_findcyanpixels_1():
-    assert type(find_red_pixels("data//map.png",upper_threshold=100,lower_threshold=50)) == np.ndarray
+    assert type(find_cyan_pixels("data//map.png",upper_threshold=100,lower_threshold=50)) == np.ndarray
+
+def test_detectconnectedcomponents_1():
+    assert find_red_pixels("data//map.png",upper_threshold=100,lower_threshold=50) != detect_connected_components(find_red_pixels("data//map.png",upper_threshold=100,lower_threshold=50))
+
+def test_detectconnectedcomponents_2():
+    assert find_cyan_pixels("data//map.png",upper_threshold=100,lower_threshold=50) != detect_connected_components(find_cyan_pixels("data//map.png",upper_threshold=100,lower_threshold=50))
+
+
