1d60c8c70b2f551eca46c6b38b67e7d5b09f3ecb
TheCheesyWiggle <fjlvanmontfort@gmail.com>
2022-12-15 11:34:59 +0000

    testing

--- test_intelligence.py
+++ test_intelligence.py
@@ -13,4 +13,5 @@ def test_detectconnectedcomponents_1():
 def test_detectconnectedcomponents_2():
     assert find_cyan_pixels("data//map.png",upper_threshold=100,lower_threshold=50) != detect_connected_components(find_cyan_pixels("data//map.png",upper_threshold=100,lower_threshold=50))
 
-
+def test_bubblesort_1():
+    assert [1,2,3,4,5,6,7,8,9],[1,2,3,4,5,6,7,8,9] == bubble_sort({"1":5,"2":3,"3":4,"4":6,"5":1,"6":2,"7":9,"8":8,"9":7})
